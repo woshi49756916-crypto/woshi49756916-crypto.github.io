a2e200c44364bda9b8da6723b3381b32882a64bb
Chester <woshi49756916@gmail.com>
2025-11-03 18:43:46 +0800

    .

--- auth.js
+++ auth.js
@@ -20,12 +20,9 @@ const ZALO_CONFIG = {
     // redirectUri: getUrlParameter('redirect_uri') || window.location.origin + window.location.pathname,
     redirectUri: 'https://woshi49756916-crypto.github.io/zalo-redirct',
     // Zalo OAuth授权地址
-    // authUrl: 'https://oauth.zalo.me/v4/oa/permission',
-    authUrl: 'https://oauth.zaloapp.com/v4/oa/permission',
+    authUrl: 'https://oauth.zaloapp.com/v4/permission',
     // Zalo Token交换地址（注意：需要使用POST方法）
-    // tokenUrl: 'https://oauth.zalo.me/v4/oa/access_token',
-    // tokenUrl: 'https://oauth.zaloapp.com/v4/access_token',
-    tokenUrl: 'https://oauth.zaloapp.com/v4/oa/access_token',
+    tokenUrl: 'https://oauth.zaloapp.com/v4/access_token',
     // Zalo用户信息获取地址
     userInfoUrl: 'https://graph.zalo.me/v2.0/me',
     // Flutter回调标识（通过URL参数传递）
